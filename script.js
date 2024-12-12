08d377c9f1695c35de7d40b71fb997da916e1270
Calandda <A.calandda@gmail.com>
2024-12-12 18:26:26 +0800

    Added Javascript Book Object

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('test');
+const myLibary = [];
+
+function Book(title, author, pages, read){
+	this.title = title;
+	this.author = author;
+	this.page = pages;
+	this.read = read;
+}
+
+function addBookToLibrary(){
+
+}
